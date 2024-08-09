359711ddda6fef22271e7946f9e1c489de5a15c1
neeraj-0340 <neerajap777@gmail.com>
2024-08-09 15:45:15 +0530

    Add files via upload

---
+++ logical test/string manipulation/string manipulation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i, n;
+    char s[100];
+    char ch[100];
+    printf("\n Enter the String : ");
+    scanf("%[^\n]%*c", s);
+    n = strlen(s);
+
+    printf("\n enter character : ");
+    scanf(" %c", &ch);
+
+    for (i = 0; i < n; i++)
+    {
+        if (s[i] != ch[0])
+        {
+            printf("%c", s[i]);
+        }
+    }
+
+    return 0;
+}
